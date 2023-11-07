9d05fdaed792d666f228465c64302518a9a3a37f
lemonmungchi <khc112353@gmail.com>
2023-11-07 21:39:27 +0900

    프로그래머스 1일차
    프로그래머스 1일차

---
+++ programmers/programmers.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(void) {
+    string str;
+    int n;
+    cin >> str;
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        cout << str;
+    }
+    return 0;
+}
