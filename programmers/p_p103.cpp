713fa8725fd8747a07ae044210ba1a656eeadc10
lemonmungchi <khc112353@gmail.com>
2024-02-19 22:16:07 +0900

    기초부터 다시시작
    기초부터 다시시작

---
+++ programmers/p_p103.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(string s) {
+    int answer = 0;
+    if (s[0] == '-') {
+        answer = stoi(s.substr(1, s.size() - 1)) * -1;
+    }
+    else {
+        answer = stoi(s);
+    }
+
+    return answer;
+}
