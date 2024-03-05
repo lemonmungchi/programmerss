e2eb04689c808cd8405749f1dcb0f6311114adbf
lemonmungchi <khc112353@gmail.com>
2024-03-05 21:38:39 +0900

    3.5
    생각을 분발하자

---
+++ programmers/p_p132.cpp
@@ -0,0 +1,52 @@
+#include <string>
+#include <cmath>
+using namespace std;
+
+int solution(string dartResult) {
+    int answer = 0;
+    int prev = 0, score = 0;
+
+
+    for (int i = 0; i < dartResult.size(); i++)
+    {
+        //숫자 분리
+        if (dartResult[i] >= '0' && dartResult[i] <= '9')
+        {
+            prev = score;         //보너스 계산을 위해 
+
+            if (dartResult[i + 1] == '0')
+            {
+                score = 10;
+                i++;
+            }
+            else {
+                score = dartResult[i] - '0';
+            }
+        }
+        else if (dartResult[i] == 'S' || dartResult[i] == 'D' || dartResult[i] == 'T')
+        {
+            if (dartResult[i] == 'D') {
+                score = pow(score, 2);
+            }
+            else if (dartResult[i] == 'T') {
+                score = pow(score, 3);
+            }
+
+            if (dartResult[i + 1] == '*')
+            {           //보너스 점수 계산
+                answer -= prev;
+                prev *= 2;
+                score *= 2;
+                i++;
+                answer += prev;
+            }
+            else if (dartResult[i + 1] == '#') {
+                score *= -1;
+                i++;
+            }
+            answer += score;
+        }
+
+    }
+    return answer;
+}
