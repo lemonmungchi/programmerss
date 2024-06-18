b81eae476601ee5daf2d1d2ab1d2896af6ed57c1
Hyeon Chan <116485176+lemonmungchi@users.noreply.github.com>
2024-06-18 22:53:06 +0900

    6.18

---
+++ programmers/p_p184.cpp
@@ -0,0 +1,25 @@
+#include <string>
+#include <vector>
+//순회하면서 자르는 수를 증가시키면서 문자열 비교
+using namespace std;
+int solution(string s) {
+    int answer = s.size();
+    for (int i = 1; i <= s.size() / 2; i++) {                //i는 문자열을 자르는 길이
+        int cnt = 1;                  //겹치는거 숫자
+        string tmp = "", a = "";        //압축된 숫자, 자른 숫자
+        a = s.substr(0, i);
+        for (int j = i; j < s.size(); j += i) {           //자른만큼 순회
+            if (a == s.substr(j, i)) cnt++;             //만약 겹치는게 있다면 
+            else {
+                if (cnt > 1) tmp += to_string(cnt);
+                tmp += a;
+                a = s.substr(j, i);                //압축될부분 잘라서 앞에다가 넣어줬기때문에 자르기
+                cnt = 1;                  //압축했으면 다시 겹치는거 찾기위해 1로 초기화
+            }
+        }
+        if (cnt > 1) tmp += to_string(cnt);
+        tmp += a;
+        if (answer > tmp.size())answer = tmp.size();
+    }
+    return answer;
+}
